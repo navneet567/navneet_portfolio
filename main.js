69074945eaba269d16fad1510d0dbf4530cff658
navneet567 <navneetsinghcreation@gmail.comn>
2026-01-14 02:01:01 +0530

    website updated from VS Code

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 
 var typed = new Typed(".text",{
-    strings :["Backend Developer" , "Web Developer"],
+    strings :["Java Developer" , "MERN Stack Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 })
+
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle('active');
+};
