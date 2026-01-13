dc059975013db74dcd6c94ade3171d02d60caebd
navneet567 <navneetsinghcreation@gmail.comn>
2026-01-13 08:37:08 +0530

    website updated from VS Code

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var typed= new typed(".text", {
-    String: ["Fronted Developer", "Youtuber", "Web Developer"],
+
+var typed = new Typed(".text",{
+    strings :["Backend Developer" , "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-});
+})
